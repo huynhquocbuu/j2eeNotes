21dcabd42f1f992718086a04f2a14be6a830cd6d
huynhquocbuu <huynhquocbuu@gmail.com>
2021-11-29 16:49:39 +0700

    coomit 4

--- src/main/java/com/example/j2eenotes/util/UrlConstants.java
+++
@@ -1,6 +0,0 @@
-package com.example.j2eenotes.util;
-
-public class UrlConstants {
-    public static final String All = "/*";
-    public static final String Welcome = "/welcome";
-}
--- src/main/java/com/example/j2eenotes/util/ViewConstants.java
+++
@@ -1,5 +0,0 @@
-package com.example.j2eenotes.util;
-
-public class ViewConstants {
-    public static final String WelcomeIndex = "/views/welcome/index.jsp";
-}
